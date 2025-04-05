2cb98c1472b4ac2d562caff06eb9aabc26997120
daniel.vishna <daniel.vishna@gmail.com>
2025-04-05 17:03:03 +0300

    Add test artifact upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def greet():
     return "Hello from CI/CD!"
 
-if __name__ == "__main__":
-    print(greet())
+def add(a, b):
+    return a + b
--- test_main.py
+++ test_main.py
@@ -1,6 +1,10 @@
-from main import greet
+from main import greet, add
 
 def test_greet():
     assert greet() == "Hello from CI/CD!"
 
+def test_add():
+    assert add(2, 3) == 5  # This should pass
 
+    # Try this next to make it fail and see behavior:
+    # assert add(2, 3) == 6  # This will FAIL
